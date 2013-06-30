5571945b25c277f7fff161b1380906715e325747
John Arnold <john@islandofdoom.com>
2013-06-30 15:55:02 -0300

    Remove error

--- EDSunriseSet.h
+++ EDSunriseSet.h
@@ -18,10 +18,6 @@
 
 #import <Foundation/Foundation.h>
 
-#if ! __has_feature(objc_arc)
-#error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag in this file.
-#endif
-
 @interface EDSunriseSet : NSObject {
 }
 
